b997512d36970312f3d936b0e72ddedd6e14d475
Tanner Yost <tyost@csumb.edu>
2019-05-08 13:53:26 -0700

    Created a demo spotify API script just to test Spotipy module. credentials.py will be added to .gitignore if repository goes public.

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import ../spotify/spotify as sp
 app = Flask(__name__)
 
 @app.route('/')
--- spotify/spotify.py
+++ spotify/spotify.py
@@ -1,37 +1,19 @@
-# import pprint
-# import sys
-
-# import spotipy
-# import spotipy.util as util
-
-# client_id = 'cf8915522beb40e0892bb9a05ebe9459' # Your client id
-# client_secret = '05ca767fb8824188b731d68999ceadec' # Your secret
-# redirect_uri = 'http://localhost:5000/' # Your redirect uri
-
-
-# if len(sys.argv) > 3:
-#     username = sys.argv[1]
-#     playlist_id = sys.argv[2]
-#     track_ids = sys.argv[3:]
-# else:
-#     print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
-#     sys.exit()
-
-# scope = 'playlist-modify-public'
-# token = util.prompt_for_user_token(username, scope)
-
-# if token:
-#     sp = spotipy.Spotify(auth=token)
-#     sp.trace = False
-#     results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
-#     print(results)
-# else:
-#     print("Can't get token for" + username)
-
-
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
-sp = spotipy.Spotify()
+import pprint
+import credentials # external credentials module to hide sensitive OAuth information
 
-results = sp.search(q='weezer', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-    print(' ', i, t['name'])
+if len(sys.argv) > 1:
+    search_str = sys.argv[1]
+else:
+    search_str = 'Radiohead'
+
+client_id = credentials.client_id # create 
+client_secret = credentials.client_secret
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+result = sp.search(search_str)
+pprint.pprint(result)
